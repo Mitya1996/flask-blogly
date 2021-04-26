be35f1c0112e71d51db1ba3ac596194239c2c20c
mits <dmitrybaycharov@gmail.com>
2021-04-26 11:05:57 -0500

    flask app skeleton creation attempt

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
+
+# DebugToolbarExtension code str8 from docs
+app.debug = True
+app.config['SECRET_KEY'] = 'secret'
+toolbar = DebugToolbarExtension(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def homepage():
+    render_template('index.html')
